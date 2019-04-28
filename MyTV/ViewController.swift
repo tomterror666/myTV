bbf5e6c5af881c556b952d6bef41436da95647f3
Andre Heß <hess.andre@gmx.net>
2019-04-28 13:52:47 +0200

    Initial Commit

---
+++ MyTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTV
+//
+//  Created by Andre Heß on 28.04.19.
+//  Copyright © 2019 Andre Heß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyTVTests/MyTVTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyTVTests.swift
+//  MyTVTests
+//
+//  Created by Andre Heß on 28.04.19.
+//  Copyright © 2019 Andre Heß. All rights reserved.
+//
+
+import XCTest
+@testable import MyTV
+
+class MyTVTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
